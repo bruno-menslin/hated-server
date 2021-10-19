3bf537435863f087e3a21e5a4b5d5c0f65482203
Bruno Menslin <brunomenslin@outlook.com>
2021-10-18 22:15:41 -0300

    feat: create User controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express";
+import { UserService } from "../services/UserService";
+
+class UserController {
+    async create(request: Request, response: Response) {
+        
+        const {username, email, password, admin} = request.body;
+
+        const userService = new UserService();
+
+        const user = await userService.create({username, email, password, admin});
+
+        return response.json(user);
+    }
+}
+
+export { UserController };
