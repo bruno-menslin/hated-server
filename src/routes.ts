82b637d7f2b3505a5eabdaf4c1569a9d9dd6c770
Bruno Menslin <brunomenslin@outlook.com>
2021-10-21 22:51:59 -0300

    feat: ensure user is authenticated to create spot features

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { AuthenticationController } from "./controllers/AuthenticationController
 import { FeatureController } from "./controllers/FeatureController";
 import { UserController } from "./controllers/UserController";
 
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+
 const router = Router();
 
 const userController = new UserController();
@@ -13,6 +15,6 @@ router.post('/login', authenticationController.login);
 
 router.post('/users', userController.create);
 
-router.post('/features', featureController.create)
+router.post('/features', ensureAuthenticated, featureController.create);
 
 export { router };
