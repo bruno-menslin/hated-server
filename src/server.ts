b0cad8dedbb26fb2d525cc67f20af43b9b97e69b
Bruno Menslin <brunomenslin@outlook.com>
2021-11-30 22:43:41 -0300

    build: add cors dependency

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
-
+import cors from "cors";
 import {router} from "./routes";
 
 import "./database";
@@ -9,7 +9,7 @@ import "./database";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 app.use(
